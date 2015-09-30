d04a41f8a86723a2d4d236d42a3b277a3aa05e32
vinifig <vinifig@hotmail.com>
2015-09-29 21:33:13 -0300

    Pagina de usuarios e projetos

--- themes/my_theme/template-projetos.php
+++ themes/my_theme/template-projetos.php
@@ -5,10 +5,28 @@ Template Name: Projetos
 
 get_header();
 ?>
-<div class="clear"></div>
+<section style="background:rgba(0,0,0,0.4); width:100vw; height:80vh;"></section>
+<style>
+	<?php
+		$image_id = get_post_thumbnail_id();
+		$image_url_big = wp_get_attachment_image_src($image_id,'post-thumbnail-large', true);
+	?>
+	body{
+		background-image: url("<?php echo get_template_directory_uri(); ?>/images/bg.jpg") ;
+		background-size: 120vw ;
+		background-position: center center;
+		background-attachment: fixed !important;
+		background-repeat: no-repeat;
+	}
+	.site-content{
+		background:transparent;
+	}
+	
+</style>
 
 </header> <!-- / END HOME SECTION  -->
 
+<div class="white-block">
 <div id="content" class="site-content">
 
 	<div class="container">
@@ -17,26 +35,11 @@ get_header();
 
 			<div id="primary" class="content-area">
 				<main id="main" class="site-main" role="main">
-					<h1>Projetos</h1>
-					<?php
-					$paged = (get_query_var('paged')) ? get_query_var('paged') : 1;			
-					query_posts(array( 'post_type' => 'post', 'cat' => get_category_by_slug('projetos')->term_id, 'posts_per_page' => get_option('posts_per_page',10), 'order' => 'DESC', 'paged' => $paged ));
-					if( have_posts() ):
-					 
-						while ($wp_query->have_posts()) : 
-						
-							the_post();
-							get_template_part( 'content-large', get_post_format() );
-
-						endwhile;
-
-					endif;
-
-					zerif_paging_nav();
-
-					wp_reset_postdata();
-					?>
-					
+					<div class="section-header">
+						<h2 class="dark-text">Projetos</h2>
+						<h6 class="dark-text">Lorem ipsum solem dolor sit amet. bacon dedeise solem</h6>
+					</div>
+					<p class="section-header-description">Nulla facilisi. Vestibulum vestibulum magna consequat nisi bibendum ullamcorper. Sed iaculis id nibh eget tincidunt. Nam laoreet lectus at dui ornare, ultricies euismod lacus porttitor. Nullam fringilla volutpat tortor, in iaculis purus gravida id. </p>
 				</main><!-- #main -->
 
 			</div><!-- #primary -->
@@ -44,6 +47,33 @@ get_header();
 		</div><!-- .content-left-wrap -->
 
 	</div><!-- .container -->
-<?php
-get_footer();
-?>
+	<div class="container-projetos projs-full-width">
+		<?php 
+				$args = array( 'post_type' => 'post', 'cat' => get_category_by_slug('projetos')->term_id,'order' => 'DESC');
+				$loop = new WP_Query( $args );
+				$i= 0;
+				while ( $loop->have_posts() ) : $loop->the_post(); ?>
+		<a href="<?php the_permalink();?>" class="projeto"  title="<?php the_title(); ?>">
+			<div class="projectBackground" style="background-image:url('<?php echo wp_get_attachment_thumb_url( get_post_thumbnail_id( $post->ID ) ); ?>')"></div>
+			<h6 class="projeto-titulo"><?php the_title(); ?></h6>
+		</a>
+		<?php
+				endwhile;
+		?>
+	</div>
+	<div class="container-parceiros">
+		<div class="section-header">
+			<h2 class="dark-text">Parceiros</h2>
+			<h6 class="dark-text">Lorem ipsum solem dolor sit amet. bacon dedeise solem</h6>
+		</div>
+		<p class="section-header-description">
+			<img src="http://127.0.0.1/wordpress/wp-content/uploads/2015/09/samsung.png" class="parceiroImg">
+			<img src="http://127.0.0.1/wordpress/wp-content/uploads/2015/09/android.png" class="parceiroImg">
+			<img src="http://127.0.0.1/wordpress/wp-content/uploads/2015/09/microsoft.png" class="parceiroImg">
+		</p>
+		<p style="font-size:0.6rem;margin-bottom:20;font-style:italic">Todas as logos e marcas são propriedade de suas respectivas empresas</p>
+	</div>
+	<?php			
+		wp_reset_postdata();				
+		get_footer();
+	?>
--- themes/my_theme/template-usuarios.php
+++ themes/my_theme/template-usuarios.php
@@ -5,10 +5,28 @@ Template Name: Equipe
 
 get_header();
 ?>
-<div class="clear"></div>
+<section style="background:rgba(0,0,0,0.4); width:100vw; height:80vh;"></section>
+<style>
+	<?php
+		$image_id = get_post_thumbnail_id();
+		$image_url_big = wp_get_attachment_image_src($image_id,'post-thumbnail-large', true);
+	?>
+	body{
+		background-image: url("<?php echo get_template_directory_uri(); ?>/images/bg.jpg") ;
+		background-size: 120vw ;
+		background-position: center center;
+		background-attachment: fixed !important;
+		background-repeat: no-repeat;
+	}
+	.site-content{
+		background:transparent;
+	}
+	
+</style>
 
 </header> <!-- / END HOME SECTION  -->
 
+<div class="white-block">
 <div id="content" class="site-content">
 
 	<div class="container">
@@ -17,10 +35,14 @@ get_header();
 
 			<div id="primary" class="content-area">
 				<main id="main" class="site-main" role="main">
-					<h1>Nossa Equipe</h1>
-					<?php
+					<div class="section-header">
+						<h2 class="dark-text">Equipe</h2>
+						<h6 class="dark-text">Lorem ipsum solem dolor sit amet. bacon dedeise solem</h6>
+					</div>
+					<div class="section-header-description"><?php
 					MyUsersClass::listaUsuarios((new WP_User_Query( array( 'meta_key' => MyUsersClass::USER_IS_ACTIVE, 'meta_value' => 'on' , "orderby" => 'name' ) ))->results);
 					?>
+					</div>
 				</main><!-- #main -->
 
 			</div><!-- #primary -->
@@ -28,6 +50,6 @@ get_header();
 		</div><!-- .content-left-wrap -->
 
 	</div><!-- .container -->
-<?php
-get_footer();
-?>
+	<?php			
+		get_footer();
+	?>
